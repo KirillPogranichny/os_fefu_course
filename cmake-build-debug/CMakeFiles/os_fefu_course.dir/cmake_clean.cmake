cd99764c862aa4120b8f7105ab27e46fd050ba8f
KirillPogranichny <kirillpogranichny@gmail.com>
2023-12-24 15:23:14 +1000

    create script for linux

---
+++ cmake-build-debug/CMakeFiles/os_fefu_course.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/os_fefu_course.dir/lab1/hello_world.cpp.o"
+  "CMakeFiles/os_fefu_course.dir/lab1/hello_world.cpp.o.d"
+  "os_fefu_course"
+  "os_fefu_course.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/os_fefu_course.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
